d15e408b4fde09a3f3e2d7d8a4621694a5bd57c7
GreySql <greysql0098@gmail.com>
2025-12-18 18:40:30 +0800

    PromptTemplate 案例补充

--- src/main/java/spring/ai/example/spring_ai_demo/prompt/PromptService.java
+++ src/main/java/spring/ai/example/spring_ai_demo/prompt/PromptService.java
@@ -1,9 +1,19 @@
 package spring.ai.example.spring_ai_demo.prompt;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.template.st.StTemplateRenderer;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,6 +22,14 @@ import java.util.Map;
  */
 @Service
 public class PromptService {
+
+    @Resource
+    private ChatModel chatModel;
+
+    @PostConstruct
+    public void init() {
+//        chatPrompt();
+    }
 
     /**
      * stringTemplate 渲染器
@@ -25,6 +43,29 @@ public class PromptService {
         // 渲染
         promptTemplate.render(Map.of("input", "hello world"));
 
+    }
+
+    public void chatPrompt() {
+        String userText = """
+                告诉我关于 ai 未来应用的方向，比如：{userInput1}、{userInput2}
+                """;
+
+        PromptTemplate userPromptTemplate = new PromptTemplate(userText);
+        Message userMessage = userPromptTemplate.createMessage(Map.of("userInput1", "医药", "userInput2", "消费"));
+
+        String systemText = """
+                你是一个 {systemInput} 助手，请根据用户输入给出一个 ai 应用方向，请使用中文回答。
+                """;
+
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemText);
+
+        Message systemMessage = systemPromptTemplate.createMessage(Map.of("systemInput", "ai"));
+
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+
+        List<Generation> results = chatModel.call(prompt).getResults();
+
+        System.out.println(results);
     }
 
 }
