8c967798b4666c3556a9183be480413b08768f9f
GreySql <greysql0098@gmail.com>
2026-02-27 12:30:17 +0800

    剔除无用的配置文件，增加注释

--- src/main/java/spring/ai/example/spring_ai_demo/configuartion/ChatClientConfig.java
+++ src/main/java/spring/ai/example/spring_ai_demo/configuartion/ChatClientConfig.java
@@ -22,6 +22,29 @@ import spring.ai.example.spring_ai_demo.tool.AiTool;
 
 /**
  * 多数情况下都会使用多个模型，所以需要禁用 chatClient 的自动配置
+ * 有多个模型配置时，建议从 db 中读取并统一初始化，一般参数有
+ * - 模型名称
+ * - 模型id
+ * - 平台 apikey
+ * - 模型平台 api 地址
+ * - 调用接口（这个根据平台不同，实际调用时接口名称不固定，建议灵活些，不过一般遵守 openai 的话接口名称一样，主要区别在于 api 地址拼接时会出现问题）
+ * - 输入 token 计费
+ * - 输出 token 计费
+ * - 图像 token 计费
+ * - 音频 token 计费
+ * - tool 使用（有些模型可能并不支持 tool 工具使用）
+ * - json 格式返回（新模型支持概率较高，旧模型或小模型可能并不支持）
+ * ### 以上计费一般只用记录输入、输出 token，特殊模型特殊处理
+ * - 模型特征（该模型擅长哪方面的）
+ * ### 特征细分，可以根据模型特征进行细分
+ * - 编码、深度思考（擅长复杂任务）、翻译、图像
+ * ###
+ * - 免费、付费
+ * - qps 限制（一般没有限制，但实际调用时免费模型一般经不起高频调用，建议 1qps）
+ *
+ * 模型有输入、输出 token 区别，定价一般不同，以 money/M tokens 为单位，m 为百万 token
+ * token 为模型计算 token 的数量，输入的问题等会统一转换成token，模型转换 token 的数量会因模型而异
+ *
  */
 @Configuration
 public class ChatClientConfig {
@@ -32,7 +55,7 @@ public class ChatClientConfig {
     @Value("${siliconflow.base-url}")
     private String siliconflowBaseUrl;
 
-    @Value("${openRouter.model}")
+    @Value("${openRouter.model:arcee-ai/trinity-large-preview:free}")
     private String openRouterModel;
 
     @Value("${openRouter.openai.api-key}")
